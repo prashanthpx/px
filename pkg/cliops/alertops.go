05d64c10db4e0fb62dcaf0af4d8cc52b23cf1eb8
Prashanth Kumar <prashanth.kumar@portworx.com>
2019-09-10 14:40:42 +0530

    Adding alerts functionality to fetch portworx alerts

---
+++ pkg/cliops/alertops.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2019 Portworx
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cliops
+
+import (
+	"github.com/portworx/pxc/pkg/portworx"
+	"github.com/portworx/pxc/pkg/util"
+	"github.com/spf13/cobra"
+)
+
+type CliAlertInputs struct {
+	util.BaseFormatOutput
+	Wide      bool
+	AlertType string
+}
+
+type CliAlertOps struct {
+	CliAlertInputs
+	PxAlertOps portworx.PxAlertOps
+}
+
+func GetCliAlertInputs(cmd *cobra.Command, args []string) *CliAlertInputs {
+	output, _ := cmd.Flags().GetString("output")
+	alertType, _ := cmd.Flags().GetString("type")
+	return &CliAlertInputs{
+		BaseFormatOutput: util.BaseFormatOutput{
+			FormatType: output,
+		},
+		AlertType: alertType,
+	}
+}
+
+// Create a new cliAlertOps object
+func NewCliAlertOps(
+	cvi *CliAlertInputs,
+) *CliAlertOps {
+	return &CliAlertOps{
+		CliAlertInputs: *cvi,
+	}
+}
